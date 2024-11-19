e1c8183c071b7fd44a2efa885dc02dddc373bb42
Klebson Motta <168038883+Binhormb@users.noreply.github.com>
2024-11-19 17:41:04 -0300

    Delete Portfólio Klebson Motta directory

--- Portfólio Klebson Motta/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".accordion").forEach(button => {
-    button.addEventListener("click", function() {
-        const panel = this.nextElementSibling; // Seleciona o próximo elemento (o painel)
-        panel.style.display = panel.style.display === "block" ? "none" : "block"; // Alterna a exibição
-    });
-});
